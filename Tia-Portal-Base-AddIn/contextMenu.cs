86174153cf2e17b61a33d472fcc2f0a52949e12d
RV1989 <rien__verbeke@msn.com>
2019-12-17 10:33:24 +0100

    first commit

---
+++ Tia-Portal-Base-AddIn/contextMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Siemens.Engineering;
+using Siemens.Engineering.AddIn;
+using Siemens.Engineering.AddIn.Menu;
+using System.Diagnostics;
+
+
+
+
+
+namespace Tia_Portal_Base_AddIn
+{
+    public class contextMenu : ContextMenuAddIn
+        
+
+    {
+
+
+        public contextMenu(string displayName) : base(displayName)
+
+        {
+
+        }
+         
+
+        protected override void BuildContextMenuItems(ContextMenuAddInRoot addInRootSubmenu)
+
+        {
+
+            addInRootSubmenu.Items.AddActionItem<IEngineeringObject>("Entry 1", Entry1_OnClick);
+
+            addInRootSubmenu.Items.AddActionItem<IEngineeringObject>("Entry 2", Entry2_OnClick);
+
+        }
+
+
+        private void Entry1_OnClick(MenuSelectionProvider<IEngineeringObject> menuSelectionProvider)
+
+        {
+            Debugger.Launch();
+            Console.WriteLine("entry1");
+            
+            //Executed program code for the first entry of the shortcut menu 
+
+        }
+
+
+        private void Entry2_OnClick(MenuSelectionProvider<IEngineeringObject> menuSelectionProvider)
+
+        {
+            Debugger.Launch();
+            Console.WriteLine("entry2");
+            //Executed program code for the second entry of the shortcut menu
+
+        }
+
+    }
+
+}
---
+++ Tia-Portal-Base-AddIn/viewProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Siemens.Engineering.AddIn;
+using Siemens.Engineering.AddIn.Menu;
+
+namespace Tia_Portal_Base_AddIn
+{
+    public class projectTreeViewProvider : ProjectTreeAddInProvider
+    {
+        protected override IEnumerable<ContextMenuAddIn> GetContextMenuAddIns()
+
+        {
+
+            yield return new contextMenu("ProjectTreeAddInMenu");
+
+
+
+        }
+
+    }
+
+    public class globalLibraryViewProvider : GlobalLibraryTreeAddInProvider
+    {
+        protected override IEnumerable<ContextMenuAddIn> GetContextMenuAddIns()
+
+        {
+
+            yield return new contextMenu("GobalLibraryAddInMenu");
+
+
+
+        }
+
+    }
+
+    public class projectLibraryViewProvider : ProjectLibraryTreeAddInProvider
+    {
+        protected override IEnumerable<ContextMenuAddIn> GetContextMenuAddIns()
+
+        {
+
+            yield return new contextMenu("ProjectLibraryAddInMenu");
+
+
+
+        }
+
+    }
+
+    public class deviceNetworkViewProvider : DevicesAndNetworksAddInProvider
+    {
+        protected override IEnumerable<ContextMenuAddIn> GetContextMenuAddIns()
+
+        {
+
+            yield return new contextMenu("DeviceNetworkAddInMenu");
+
+
+
+        }
+
+    }
+
+   
+
+    
+}
